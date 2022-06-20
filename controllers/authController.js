ec70e6dc24bc52d196666cec87f8097713461616
Thongrob Tipsurat <thongrob.tsr@gmail.com>
2022-06-20 10:52:00 +0700

    Edit authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,32 @@
 const jwt = require("jsonwebtoken")
 
-exports.login=(req,res)=>{
-  //ข้อมูลของ user, password ที่ login เข้าสู่ระบบ
+exports.login = (req,res)=>{
+  // //ข้อมูลของ user, password ที่ login เข้าสู่ระบบ
   const {username,password} = req.body
-  if(password === process.env.PASSWORD ){
+  if(password === process.env.PASSWORD && username === process.env.USER){
     //login เข้าสู่ระบบ
     const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'1d'})
     return res.json({token,username})
+  }else if(!username || !password){
+    return( res.status(400)
+      .json({
+        error:" กรุณาใส่ User/Password"
+      })
+    )
   }else{
     return( res.status(400)
       .json({
-        error:"รหัสผ่านไม่ถูกต้อง!"
+        error:"User/Password ไม่ถูกต้อง!"
       })
     )
   }
+
   // //ทดสอบให้ response useername, password
   // res.json({
-  //   username,password
-  // })
+  //   data:"login test"
+ 
+  //ทดสอบให้ response useername, password
+  res.json({
+    username, password
+  })
 }
